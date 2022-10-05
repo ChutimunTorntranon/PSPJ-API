ad6f56f8644789dd9bc664eb59845abbdb40b75c
ChutimunTorntranon <ChutimunTorntranon@gamil.com>
2022-10-05 21:40:24 +0700

    sign up success

--- src/app.js
+++ src/app.js
@@ -7,19 +7,22 @@ const error = require('./middlewares/error');
 
 const app = express(); // ประกาศ app ให้ใช้ express
 
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('combined'));
+}
 // const { sequelize } = require('./models');
-// sequelize.sync({ alter: true });
+// sequelize.sync({ force: true });
+
+const authRoute = require('./routes/authRoute');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/auth', authRoute);
+
 app.use(notFound);
 app.use(error);
-
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('combined'));
-}
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`server running on port ${port}`));
---
+++ src/utils/appError.js
@@ -0,0 +1,5 @@
+module.exports = class AppError extends Error {
+	constructor(message, statusCode) {
+		super(message), (this.statusCode = statusCode);
+	}
+};
